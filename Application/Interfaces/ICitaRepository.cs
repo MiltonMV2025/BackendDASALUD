7bd7b5e874d3574ed3295a59cdbb9e81505cff69
Milton Mayorga <miltonmayorgavasquez@gmail.com>
2025-11-22 21:34:30 -0600

    add citas catalogs

--- Application/Interfaces/ICitaRepository.cs
+++ Application/Interfaces/ICitaRepository.cs
@@ -9,4 +9,7 @@ public interface ICitaRepository
     Task<bool> UpdateAsync(EditCitaDto dto, CancellationToken cancellationToken = default);
     Task<AppointmentDetailsDto?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
     Task<bool> SoftDeleteAsync(int id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<DoctorCatalogDto>> GetDoctorsCatalogAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<PatientCatalogDto>> GetPatientsCatalogAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<AppointmentStatusDto>> GetStatusesCatalogAsync(CancellationToken cancellationToken = default);
 }
